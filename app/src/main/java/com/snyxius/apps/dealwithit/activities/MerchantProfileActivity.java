06447931da314c4fc9cd3669874b07f5bdb2e6de
aman007jham <aman.jham91@gmail.com>
2015-11-13 18:49:18 +0530

    Nov 13, 2015 @ 7pm

--- app/src/main/java/com/snyxius/apps/dealwithit/activities/MerchantProfileActivity.java
+++ app/src/main/java/com/snyxius/apps/dealwithit/activities/MerchantProfileActivity.java
@@ -2,52 +2,133 @@ package com.snyxius.apps.dealwithit.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.snyxius.apps.dealwithit.R;
 
 /**
  * Created by snyxius on 11/12/2015.
  */
-public class MerchantProfileActivity extends AppCompatActivity {
+public class MerchantProfileActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
 
-    AppBarLayout mAppBar;
-    private static final float THRESHOLD_PERCENTAGE = 0.2F;
-    ImageView proImage;
+    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR  = 0.9f;
+    private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS     = 0.3f;
+    private static final int ALPHA_ANIMATIONS_DURATION              = 200;
+
+    private boolean mIsTheTitleVisible          = false;
+    private boolean mIsTheTitleContainerVisible = true;
+
+    private LinearLayout mTitleContainer;
+    private TextView mTitle;
+    private AppBarLayout mAppBarLayout;
+    private ImageView mImageparallax;
+    private FrameLayout mFrameParallax;
+    private Toolbar mToolbar;
+//    ImageView proImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.merchant_profile_layout);
 
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        proImage=(ImageView)findViewById(R.id.pro_image);
-        setSupportActionBar(toolbar);
-        toolbar.setTitle("Nihas");
+        bindActivity();
+
+        mToolbar.setTitle("");
+//        proImage=(ImageView)findViewById(R.id.pro_image);
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
+//        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        startAlphaAnimation(mTitle, 0, View.INVISIBLE);
+        initParallaxValues();
 
-        mAppBar = (AppBarLayout) findViewById(R.id.appbar);
-        mAppBar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-            @Override
-            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-                int maxScroll = appBarLayout.getTotalScrollRange();
-                float progressPercentage = (float) (Math.abs(verticalOffset)/maxScroll);
 
-                if (progressPercentage >= THRESHOLD_PERCENTAGE) {
-                    //start an alpha animation on your ImageView here (i.e. fade out)
-                    Animation rotate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
-                    proImage.startAnimation(rotate);
-                } else {
-                    //Add an opposite animation here (i.e. it fades back in again)
-                    Animation rotate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
-                    proImage.startAnimation(rotate);
-                }
+
+
+        mAppBarLayout.addOnOffsetChangedListener(this);
+    }
+
+    private void bindActivity() {
+        mToolbar        = (Toolbar) findViewById(R.id.toolbar);
+        mTitle          = (TextView) findViewById(R.id.main_textview_title);
+        mTitleContainer = (LinearLayout) findViewById(R.id.main_linearlayout_title);
+        mAppBarLayout   = (AppBarLayout) findViewById(R.id.appbar);
+        mImageparallax  = (ImageView) findViewById(R.id.backdrop);
+        mFrameParallax  = (FrameLayout) findViewById(R.id.main_framelayout_title);
+    }
+
+    private void initParallaxValues() {
+        CollapsingToolbarLayout.LayoutParams petDetailsLp =
+                (CollapsingToolbarLayout.LayoutParams) mImageparallax.getLayoutParams();
+
+        CollapsingToolbarLayout.LayoutParams petBackgroundLp =
+                (CollapsingToolbarLayout.LayoutParams) mFrameParallax.getLayoutParams();
+
+        petDetailsLp.setParallaxMultiplier(0.9f);
+        petBackgroundLp.setParallaxMultiplier(0.3f);
+
+        mImageparallax.setLayoutParams(petDetailsLp);
+        mFrameParallax.setLayoutParams(petBackgroundLp);
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        int maxScroll = appBarLayout.getTotalScrollRange();
+        float percentage = (float) Math.abs(verticalOffset) / (float) maxScroll;
+
+        handleAlphaOnTitle(percentage);
+        handleToolbarTitleVisibility(percentage);
+    }
+
+    private void handleToolbarTitleVisibility(float percentage) {
+        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
+
+            if(!mIsTheTitleVisible) {
+                startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleVisible = true;
             }
-        });
+
+        } else {
+
+            if (mIsTheTitleVisible) {
+                startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                mIsTheTitleVisible = false;
+            }
+        }
+    }
+
+    private void handleAlphaOnTitle(float percentage) {
+        if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
+            if(mIsTheTitleContainerVisible) {
+                startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                mIsTheTitleContainerVisible = false;
+            }
+
+        } else {
+
+            if (!mIsTheTitleContainerVisible) {
+                startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleContainerVisible = true;
+            }
+        }
+    }
+
+    public static void startAlphaAnimation (View v, long duration, int visibility) {
+        AlphaAnimation alphaAnimation = (visibility == View.VISIBLE)
+                ? new AlphaAnimation(0f, 1f)
+                : new AlphaAnimation(1f, 0f);
+
+        alphaAnimation.setDuration(duration);
+        alphaAnimation.setFillAfter(true);
+        v.startAnimation(alphaAnimation);
     }
 }
