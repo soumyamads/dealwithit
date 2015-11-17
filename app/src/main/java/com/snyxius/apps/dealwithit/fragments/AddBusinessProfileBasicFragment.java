912625b3ac8879f2c02daf7f52b3d59d045793bb
aman007jham <aman.jham91@gmail.com>
2015-11-17 18:51:46 +0530

    Nov 17, 2015 @ 7pm

--- app/src/main/java/com/snyxius/apps/dealwithit/fragments/AddBusinessProfileBasicFragment.java
+++ app/src/main/java/com/snyxius/apps/dealwithit/fragments/AddBusinessProfileBasicFragment.java
@@ -134,6 +134,7 @@ public class AddBusinessProfileBasicFragment extends Fragment implements View.On
         view.findViewById(R.id.est_type).setOnClickListener(this);
         view.findViewById(R.id.upload_menu_layout).setOnClickListener(this);
         view.findViewById(R.id.cover_image_layout).setOnClickListener(this);
+        view.findViewById(R.id.button_clear).setOnClickListener(this);
         est_type_text = (TextView)view.findViewById(R.id.est_type_text);
 //        ambience_text = (TextView)view.findViewById(R.id.ambience_text);
 //        cuisine_text = (TextView)view.findViewById(R.id.cuisine_text);
@@ -156,13 +157,9 @@ public class AddBusinessProfileBasicFragment extends Fragment implements View.On
                         .addToBackStack(null)
                         .commit();
                 break;
-//            case R.id.cuisine_layout:
-//                getActivity().getSupportFragmentManager().beginTransaction()
-//                        .setCustomAnimations(R.anim.push_up_in, R.anim.push_down_out, R.anim.push_up_in, R.anim.push_down_out)
-//                        .add(R.id.container, new CuisineTypeFragment(), Constants.CUISINE_FRAGMENT)
-//                        .addToBackStack(null)
-//                        .commit();
-//                break;
+            case R.id.button_clear:
+                mAutocompleteView.setText("");
+                break;
 //            case R.id.ambience_layout:
 //                getActivity().getSupportFragmentManager().beginTransaction()
 //                        .setCustomAnimations(R.anim.push_up_in, R.anim.push_down_out, R.anim.push_up_in, R.anim.push_down_out)
--- app/src/main/java/com/snyxius/apps/dealwithit/fragments/AddBusinessProfileDealFragment.java
+++ app/src/main/java/com/snyxius/apps/dealwithit/fragments/AddBusinessProfileDealFragment.java
@@ -7,7 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
+import com.appyvet.rangebar.RangeBar;
 import com.snyxius.apps.dealwithit.R;
 import com.snyxius.apps.dealwithit.activities.BusinessProfileActivity;
 import com.snyxius.apps.dealwithit.activities.DealWithItActivity;
@@ -19,9 +23,11 @@ import com.snyxius.apps.dealwithit.extras.Constants;
  */
 public class AddBusinessProfileDealFragment extends Fragment implements View.OnClickListener{
 //    Intent intent;
-//    EditText email,password;
+    EditText leftIndexValue,rightIndexValue;
 //    TextView fgtpaswd;
 //    Button login;
+// Initializes the RangeBar in the application
+private RangeBar rangebar;
 
     @Override
     public void onAttach(Activity activity) {
@@ -59,11 +65,31 @@ public class AddBusinessProfileDealFragment extends Fragment implements View.OnC
     public void onViewCreated(final View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initialise(view);
+        rangebar.setPinColor(getResources().getColor(R.color.colorPrimaryDark));
+        rangebar.setTickColor(getResources().getColor(R.color.grey));
+        rangebar.setConnectingLineColor(getResources().getColor(R.color.colorPrimaryDark));
+        rangebar.setBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        rangebar.setSelectorColor(getResources().getColor(R.color.colorPrimaryDark));
+        rangebar.setOnRangeBarChangeListener(new RangeBar.OnRangeBarChangeListener() {
+            @Override
+            public void onRangeChangeListener(RangeBar rangeBar, int leftPinIndex, int rightPinIndex, String leftPinValue, String rightPinValue) {
+                leftIndexValue.setText(leftPinValue);
+                rightIndexValue.setText(rightPinValue);
+            }
+        });
+
 
     }
 
     private void initialise(View view){
         view.findViewById(R.id.continue_deal).setOnClickListener(this);
+        rangebar = (RangeBar) view.findViewById(R.id.rangebar1);
+//        rangebar.setTickStart(5);
+//        rangebar.setTickInterval(1);
+//        rangebar.setTickEnd(100);
+        // Gets the index value TextViews
+        leftIndexValue = (EditText) view.findViewById(R.id.leftIndexValue);
+        rightIndexValue = (EditText) view.findViewById(R.id.rightIndexValue);
 //        email=(EditText)rootView.findViewById(R.id.email);
 //        password=(EditText)rootView.findViewById(R.id.password);
 //        fgtpaswd=(TextView)rootView.findViewById(R.id.fgtpaswd);
@@ -83,6 +109,10 @@ public class AddBusinessProfileDealFragment extends Fragment implements View.OnC
         }
 
     }
+
+
+
+
 
 
 }
